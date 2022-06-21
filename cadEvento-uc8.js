60152a23178f7537d26d9ea195c81b1659019f28
Flavia <flavia.lima643@gmail.com>
2022-06-20 22:10:50 -0300

    Retificacao

--- cadEvento-uc8.js
+++ cadEvento-uc8.js
@@ -9,7 +9,7 @@ const rs = require("readline-sync");
 let data = new Date();
 let dataEvento = new Date ('2022-06-30');
 let idade = rs.question("informe a sua idade: ");
-let qtdeVagas = 105;
+let qtdeVagas = 100;
 let nome = "Flavia";
 let sobrenome ="Lima";
 let listaParticipantes = ["Joao, Maria, Deisiane"];
@@ -21,7 +21,7 @@ if(dataEvento < data){
 }else{
     console.log("Bem- vindo a nossa página, por favor preencha os campos solicitados: ")
 }
-if( idade <= 18 || qtdeVagas >100){
+if( idade < 18 || qtdeVagas >100){
     console.log("Cadastro não realizado. Por favor, verifique a idade informado ou a quantidade de vagas disponíveis.");
 }else{
     let publico = rs.question("Voce e um participante ou palestrante? ");
